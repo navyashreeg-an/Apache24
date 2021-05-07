091285a9e4f2af2c4a4677ab281f14de44498997
navyashreeg-an <83761756+navyashreeg-an@users.noreply.github.com>
2021-05-07 12:59:29 +0530

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$db_user = "root";
-$db_pass = "Navya@1998";
-$db_name = "student";
-
-$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
